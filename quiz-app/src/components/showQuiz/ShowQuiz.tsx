3557ef0333f8371f10146c2b32ed00cb20664a6a
joakimfr <fritzjoakim@hotmail.com>
2023-09-03 17:42:12 +0200

    showQuiz

--- quiz-app/src/components/showQuiz/ShowQuiz.tsx
+++ quiz-app/src/components/showQuiz/ShowQuiz.tsx
@@ -1,11 +1,19 @@
-
+import './ShowQuiz.scss'
 
 function ShowQuiz () {
 
+  async function handleGetQuiz () {
+    const url = 'https://fk7zu3f4gj.execute-api.eu-north-1.amazonaws.com/quiz'
+
+
+  }
+
+
   return (
-    <div>
-      <section>
-        
+    <div className='showQuiz'>
+      <section className='showQuiz__container'>
+        <h2 className='showQuiz__title'>Tillängliga Quiz</h2>
+        <button onClick={handleGetQuiz}>Visa quiz</button>  
       </section>
     </div>
   )
--- quiz-app/src/views/quiz/Quiz.tsx
+++ quiz-app/src/views/quiz/Quiz.tsx
@@ -1,6 +1,7 @@
 import Header from "../../components/header/Header"
 import QuizForm from "../../components/quizForm/QuizForm"
 import MapBox from "../../components/mapBox/MapBox"
+import ShowQuiz from "../../components/showQuiz/ShowQuiz"
 
 function Quiz () {
 
@@ -13,6 +14,7 @@ function Quiz () {
       <Header />
       <QuizForm />
       <MapBox />
+      <ShowQuiz />
     </div>
   )
 
